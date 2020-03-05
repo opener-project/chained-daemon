dedb4a359ce04ad123b90d8186449e944d0cebe6
Braulio Oliveira <brauliooliveira@olery.com>
2020-03-05 10:09:04 -0300

    chained_daemon: put back original language on process end

--- lib/opener/chained_daemon/chained_daemon.rb
+++ lib/opener/chained_daemon/chained_daemon.rb
@@ -15,40 +15,69 @@ module Opener
         'opener-ner':                    Opener::Ner.new,
         'opener-opinion-detector-basic': Opener::OpinionDetectorBasic.new,
       }
-      @translator = Google::Cloud.new.translate ENV['GOOGLE_TRANSLATE_TOKEN']
+      @last = @queue_map.keys.last
     end
 
     def run input, params = {}
       params.symbolize_keys!
       params[:translate_languages] ||= []
 
+      lang     = nil
       output   = nil
       @queue_map.each do |queue, component|
         debug_print queue, input if ENV['DEBUG']
 
         output = component.run input
         input  = output
+
+        if lang and queue == @last
+          # put back original language
+          xml    = Nokogiri.parse input
+          xml.root.attributes['lang'].value = lang
+          output = xml.to_s
+        end
+
       rescue Core::UnsupportedLanguageError
         xml  = Nokogiri.parse input
         lang = xml.root.attr('xml:lang')
         raise unless lang.in? params[:translate_languages]
 
-        input  = translate input
+        input = translate input, params
         retry
       end
+
       output
+
     rescue Core::UnsupportedLanguageError
       puts "Error on unsupported language: #{input}" if ENV['DEBUG']
       output
     end
 
-    def translate input
+    def translate input, params
       xml = Nokogiri.parse input
       raw = xml.at :raw
-      raw.content = @translator.translate raw.content, to: :en
+      case translate_service params
+      when :google
+        raw.content = google_translator.translate raw.content, to: :en
+      else
+        raw.content = google_translator.translate raw.content, to: :en
+      end
+
       xml.root.attributes['lang'].value = 'en'
       xml.to_s
     end
+
+    protected
+
+    def translate_service params
+      params[:translate_service]&.to_sym || :google
+    end
+
+    def google_translator
+      @google_translator ||= Google::Cloud.new.translate ENV['GOOGLE_TRANSLATE_TOKEN']
+    end
+
+    private
 
     def debug_print queue, input
       return unless ENV['DEBUG']
