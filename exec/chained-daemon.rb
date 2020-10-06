cbc04cd41ae16051826932b8b0412fe2f30b73f7
Braulio Oliveira <brauliooliveira@olery.com>
2020-10-06 13:11:00 -0300

    exec: run puma/rack in a different thread

--- exec/chained-daemon.rb
+++ exec/chained-daemon.rb
@@ -4,5 +4,8 @@ require 'opener/daemons'
 
 require_relative '../lib/opener/chained_daemon'
 
+webservice = Rack::Server.new app: Opener::ChainedDaemon::Webservice.freeze.app
+Thread.new{ webservice.start }
+
 daemon = Opener::Daemons::Daemon.new Opener::ChainedDaemon
 daemon.start
