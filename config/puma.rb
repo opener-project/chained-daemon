0be32e55a80ca9a573cd93449a90c8ec84ffe09a
Braulio Oliveira <brauliooliveira@olery.com>
2020-11-04 10:33:47 -0300

    puma: increase threads

--- config/puma.rb
+++ config/puma.rb
@@ -3,8 +3,8 @@ deploy_to    = "/var/www/#{APP_NAME}"
 run_path     = "/var/run/#{APP_NAME}"
 log_path     = "/var/log/#{APP_NAME}"
 
-threads 0,1
+threads 0,16
 
 bind 'tcp://0.0.0.0:9292'
-bind "unix://#{run_path}/chained-daemon-server.sock"
+#bind "unix://#{run_path}/chained-daemon-server.sock"
 
