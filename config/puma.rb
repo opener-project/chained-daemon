cabcbda451ed9dcea42911966af75c29160bd396
Developer on server <development+server@olery.com>
2020-11-06 18:20:42 +0000

    Remove threads as it was causing mem conflict

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,8 @@ deploy_to    = "/var/www/#{APP_NAME}"
 run_path     = "/var/run/#{APP_NAME}"
 log_path     = "/var/log/#{APP_NAME}"
 
-threads 0,16
+workers 5
+threads 1,1
 
 bind 'tcp://0.0.0.0:9292'
 #bind "unix://#{run_path}/chained-daemon-server.sock"
