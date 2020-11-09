b61e4b5953be1b90a34eac95483898a1f82d6ead
Braulio Oliveira <brauliooliveira@olery.com>
2020-11-09 09:17:22 -0300

    puma: use threads again

--- config/puma.rb
+++ config/puma.rb
@@ -3,8 +3,7 @@ deploy_to    = "/var/www/#{APP_NAME}"
 run_path     = "/var/run/#{APP_NAME}"
 log_path     = "/var/log/#{APP_NAME}"
 
-workers 5
-threads 1,1
+threads 0,32
 
 bind 'tcp://0.0.0.0:9292'
 #bind "unix://#{run_path}/chained-daemon-server.sock"
