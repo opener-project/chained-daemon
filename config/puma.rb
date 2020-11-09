6e7655bf71099d2eaf44805739872bc7ff406b6d
Braulio Oliveira <brauliooliveira@olery.com>
2020-11-09 10:34:20 -0300

    puma: use threads/workers from env

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,8 @@ deploy_to    = "/var/www/#{APP_NAME}"
 run_path     = "/var/run/#{APP_NAME}"
 log_path     = "/var/log/#{APP_NAME}"
 
-threads 0,32
+workers    ENV['WORKERS']&.to_i || 1  unless RUBY_ENGINE == 'jruby'
+threads 0, ENV['THREADS']&.to_i || 16
 
 bind 'tcp://0.0.0.0:9292'
 #bind "unix://#{run_path}/chained-daemon-server.sock"
