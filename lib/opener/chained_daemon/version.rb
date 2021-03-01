6062c6d6583e3b0624f52639b93904f4077c970e
Hugo Melo <hugomelo@olery.com>
2021-03-01 11:39:17 -0300

    Bump version to 3.3.9

--- lib/opener/chained_daemon/version.rb
+++ lib/opener/chained_daemon/version.rb
@@ -1,11 +1,7 @@
 module Opener
   class ChainedDaemon
 
-<<<<<<< HEAD
-    VERSION = '3.3.5'
-=======
-    VERSION = '3.3.8'
->>>>>>> 617aafa65a550e6f7a3ef0917c57aee15cacfce0
+    VERSION = '3.3.9'
 
   end
 end
